0c01093b900cb5da25780e0e7bccf1f3150218ab
PARIS Xanthin <x.paris@rubika-edu.com>
2021-04-01 15:32:51 +0200

    Clean

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : ScriptableObject
+{
+    public bool isReusable;
+    public int reusableTime;
+
+    public string skillName;
+    public string skillDescription;
+    public bool isBig;
+
+    public enum conditionType {minValue,maxValue,countdown,pair,impair,value}
+
+    public abstract void Use();
+
+}
